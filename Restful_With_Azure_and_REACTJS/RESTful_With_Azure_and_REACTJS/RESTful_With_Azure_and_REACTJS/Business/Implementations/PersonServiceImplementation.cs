ca0a67bdfbbaab1c3dee36f15070f37630f81d24
Iranildo Júnior <iranildo@outlook.com>
2022-04-01 22:50:30 -0300

    Correção da camada de Bussiness com o Repository

--- Restful_With_Azure_and_REACTJS/RESTful_With_Azure_and_REACTJS/RESTful_With_Azure_and_REACTJS/Business/Implementations/PersonServiceImplementation.cs
+++ Restful_With_Azure_and_REACTJS/RESTful_With_Azure_and_REACTJS/RESTful_With_Azure_and_REACTJS/Business/Implementations/PersonServiceImplementation.cs
@@ -1,97 +1,55 @@
 ﻿using RESTful_With_Azure_and_REACTJS.Model;
 using RESTful_With_Azure_and_REACTJS.Model.Context;
+using RESTful_With_Azure_and_REACTJS.Repository;
 
 namespace RESTful_With_Azure_and_REACTJS.Business.Implementations
 {
     public class PersonBusinessImplementation : IPersonBusiness // A Classe implenta a interface Person
     {
-        private MySQLContext _context;
+        private readonly IPersonRepository _repository;
 
 
-        public PersonBusinessImplementation(MySQLContext context )
+        public PersonBusinessImplementation(IPersonRepository repository )
         {
   
-            _context = context;
+            _repository = repository;
         
         }
 
         public Person Create(Person person)
         {
-            try
-            {
-                _context.Add(person);
-                _context.SaveChanges();
-            }
-            catch (Exception ex)
-            {
-
-                throw ex;
-            }
             
-            return person;
+            return _repository.Create(person);
         }
 
         public void Delete(long id)
         {
-           var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+          _repository.Delete(id);
 
-            if (result != null)
-            {
-                try
-                {
-                    _context.Persons.Remove(result);
-                    _context.SaveChanges();
-                }
-                catch (Exception)
-                {
-
-                    throw;
-                }
-
-            }
+            
         }
 
         public List<Person> FindAll()
         {
 
             
-            return _context.Persons.ToList();
+            return _repository.FindAll();
         }
 
         
 
         public Person FindById(long id)
         {
-            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            return _repository.FindById(id);
 
 
         }
 
         public Person Update(Person person)
         {
-            if (!Exists(person.Id)) return new Person();
-            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
-            if (result != null)
-            {
-                try
-                {
-                    _context.Entry(result).CurrentValues.SetValues(person);
-                    _context.SaveChanges();
-                }
-                catch (Exception)
-                {
-
-                    throw;
-                }
-
-            }
-
-            return person;
+            
+            return _repository.Update(person);
         }
 
-        private bool Exists(long id)
-        {
-            return _context.Persons.Any(p => p.Id.Equals(id));
-        }
     }
 }
